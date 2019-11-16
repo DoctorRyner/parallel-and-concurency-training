5af02b4cdd9651d9252254d098cc28e1efa3bb19
Ryner Gabriel <drryner@icloud.com>
2019-11-16 19:39:09 +0300

    Fix screenshot name

--- src/Parallel/Playground.hs
+++ src/Parallel/Playground.hs
@@ -1,5 +1,6 @@
 module Parallel.Playground where
 
+import           Control.DeepSeq
 import           Control.Parallel.Strategies
 import           Data.Function               ((&))
 import           Data.List.Split
@@ -35,7 +36,7 @@ nonParallelWithoutZip = nonParallelCustom countSignChangesWithoutZip
 
 -- Parallel functions
 parallelCustom :: ([Integer] -> Int) -> Int
-parallelCustom f =  runEval $ pure $ sum $ parMap rpar f $ chunksOf chunkSize testList
+parallelCustom f =  runEval $ pure $ sum $ force $ parMap rpar f $ chunksOf chunkSize testList
   where
     chunkSize = 10000
 
